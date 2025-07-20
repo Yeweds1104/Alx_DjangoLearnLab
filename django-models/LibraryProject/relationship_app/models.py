1e32e7c1a28e04ae970322009c32bf268df4224b
Yeweds Dagne <yewedsdagne@gmail.com>
2025-07-20 06:37:44 +0300

    Implementing Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -10,7 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    publication_year = models.IntegerField()
+    #publication_year = models.IntegerField()
     
     def __str__(self):
         return self.title
@@ -28,3 +29,15 @@ class Librarian(models.Model):
     
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # Create your views here.
 def list_books(request):
@@ -42,4 +44,23 @@ class LoginView(LoginView):
 class LogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
 
-  
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
