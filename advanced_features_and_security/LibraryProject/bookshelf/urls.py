61704e934c202071a2e4352294bcf7630c9347db
Yeweds Dagne <yewedsdagne@gmail.com>
2025-07-26 22:07:16 +0300

    Implement security

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('delete/<int:pk>/', views.book_delete, name='book_delete'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('form/', views.example_view, name='form_example'),
 ]
