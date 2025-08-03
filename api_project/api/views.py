b4769d4838bf5eacc4f793804e02139d772b313e
Yeweds Dagne <yewedsdagne@gmail.com>
2025-08-03 03:35:54 +0300

    viewsets and routers in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,6 +9,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
