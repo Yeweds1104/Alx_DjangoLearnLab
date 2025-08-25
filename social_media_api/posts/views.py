ecedb1bd3df177c9d92839bef0062492bd67d77e
Yeweds Dagne <yewedsdagne@gmail.com>
2025-08-25 15:26:20 +0300

    Notifications and Likes functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -51,10 +51,10 @@ class LikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def post(self, request, post_id):
-        post = get_object_or_404(Post, id=post_id)
+        post = generics.get_object_or_404(Post, id=post_id)
         if Like.objects.filter(post=post, user=request.user).exists():
             return Response({"detail": "You already likes this post."}, status=status.HTTP_400_BAD_REQUEST)
-        Like.objects.create(post=post, user=request.user)
+        Like.objects.get_or_create(post=post, user=request.user)
         
         if post.author != request.user:
             Notification.objects.create(
@@ -69,7 +69,7 @@ class UnlikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, post_id):
-        post = get_object_or_404(Post, id=post_id)
+        post = generics.get_object_or_404(Post, id=post_id)
 
         like = Like.objects.filter(post=post, user=request.user).first()
         if not like:
